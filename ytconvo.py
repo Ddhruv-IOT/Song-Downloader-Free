28cdb1d971cc0f101094a25c12fe008a22b2c71f
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-09-19 14:05:00 +0530

    changed file modes

--- ytconvo.py
+++ ytconvo.py
@@ -24,16 +24,41 @@ def video_to_mp3(input_video_path, video_name, v_no_ext, output_mp3_path):
         print(f"Video converted to MP3: {output_mp3_path + '/' + v_no_ext + '.mp3'}")
         os.remove(input_video_path + "/" + video_name)
         print("Original video file deleted")
+        return True
     except Exception as e:
         print(f"Error: {e}")
+        return False
 
 if __name__ == "__main__":
-    video_url_list = []
-    
-    with open("video_url_list.txt") as f:
-        video_url_list = f.readlines()
-
-    if video_url_list not in ["", None]:
-        for video_url in video_url_list:
-            download_path, name, name_no_ext = download_video(video_url, output_path="./z1")
-            video_to_mp3(download_path, name, name_no_ext, "./z2")
+    download_video_list = []
+    with open("video_url_list.txt", "r+") as f:
+        with open("downloaded.txt", "a+") as downloads:
+            download_urls = [line.strip() for line in downloads.readlines()]
+            video_urls = [line.strip() for line in f.readlines()]
+            
+            if video_urls == []:
+                print("Nothing to Do... Exiting...")
+                exit()
+            
+            for video_url in video_urls:
+                if video_url in download_urls:
+                    print("Nothing to do... Already resolved")
+                    download_video_list.append(video_url)
+                    continue
+                print("Downloading...")
+                download_path, name, name_no_ext = download_video(video_url, output_path="./z1")
+                print("Converting...")
+                ack = video_to_mp3(download_path, name, name_no_ext, "./z2")
+                if ack:
+                    download_video_list.append(video_url)
+                    downloads.write(video_url + "\n")
+                else:
+                    print("Error, Skipping...")
+            
+            final_list = list(set(video_urls) - set(download_video_list))
+            
+            f.seek(0)
+            
+            for url in final_list:
+                f.write(url + "\n")
+            f.truncate()
